26963f9c466d637186ee1a05fa45ea01beff490a
Miguel Alex Cantu <miguel.can2@gmail.com>
2019-09-13 13:36:59 -0500

    [wip] Implementing Login

--- carwash/carwash/settings.py
+++ carwash/carwash/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'social_django',
+    'carwash',
+    'washes'
 ]
 
 MIDDLEWARE = [
@@ -62,6 +65,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'social_django.context_processors.backends',
+                'social_django.context_processors.login_redirect'
             ],
         },
     },
@@ -99,6 +104,13 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTHENTICATION_BACKENDS = (
+ 'social_core.backends.open_id.OpenIdAuth',  # for Google authentication
+ 'social_core.backends.google.GoogleOpenId',  # for Google authentication
+ 'social_core.backends.google.GoogleOAuth2',  # for Google authentication
+ 'django.contrib.auth.backends.ModelBackend',
+)
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
@@ -118,3 +130,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Redirect the user after authenticating from Django Login and Social Auth
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'washes/'
+
+# Social auth
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = ""
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = ''
--- carwash/carwash/urls.py
+++ carwash/carwash/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.auth import views as auth_views
+from . import views
 
 urlpatterns = [
-    path('washes/', include('washes.urls')),
+    path('washes/', include('washes.urls'), name = 'washes'),
     path('admin/', admin.site.urls),
+    path('login/', auth_views.LoginView.as_view() , name = 'login'),
+    path('auth/', include('social_django.urls', namespace = 'social')),
 ]

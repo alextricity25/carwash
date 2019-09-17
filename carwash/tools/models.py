df646aa339c3eba553db5793f6dabb86bebce97e
Miguel Alex Cantu <miguel.can2@gmail.com>
2019-09-17 14:25:27 -0500

    Add models and migrations for `tools` and `washes`
    This commit adds the models and migrations for the tools and
    washes apps. Washes can have many tools associated with them,
    and each tool can be used in several washes.
    Hence, a many-to-many relationship is implemented in the model
    `Tools`, with `related_name = tools` so that it can be referred
    back to.
    
    Several other basic fields have been implemented for each model.

--- carwash/tools/models.py
+++ carwash/tools/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from washes.models import Washes
 
-# Create your models here.
+class Tools(models.Model):
+    name = models.CharField(max_length = 50)
+    brand = models.CharField(max_length = 50)
+    use = models.CharField(max_length = 50)
+    # The wash the tool is associated with. Tools can be associated
+    # with multiple washes.
+    wash = models.ManyToManyField(Washes, related_name = 'tools')
+
+    def __str__(self):
+        return self.name
+
--- carwash/washes/models.py
+++ carwash/washes/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Washes(models.Model):
+    title = models.CharField(max_length=50)
+    date_created = models.DateTimeField('date created')
+
+    def __str__(self):
+        return self.title
+    
+

13a36ba3af5934aad1f0b9fb5411bd2c2dd3814d
Miguel Alex Cantu <miguel.can2@gmail.com>
2019-09-17 14:40:02 -0500

    Added basic model for towels

--- carwash/towels/models.py
+++ carwash/towels/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Towels(models.Model):
+    name = models.CharField(max_length = 50)
+    material = models.CharField(max_length = 50)
+    gsm = models.IntegerField()
+    origin = models.CharField(max_length = 50)
+    blend_ratio = models.CharField(max_length = 50)

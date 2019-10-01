3f6b93c8bd4e62f5e996d7cc180cfe2d44fec554
Miguel Alex Cantu <miguel.can2@gmail.com>
2019-10-01 16:52:52 -0500

    Added rest-auth urls for authentication
    Users can now login, logout, and signup using the carwash REST
    API!

--- carwash/carwash/urls.py
+++ carwash/carwash/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('tools/', include('tools.urls')),
     path('washes/', include('washes.urls')),
     path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls'))
+    path('rest-auth/', include('rest_auth.urls')),
+    path('rest-auth/registration/', include('rest_auth.registration.urls'))
 ]

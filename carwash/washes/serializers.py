aa7c89999dd746185ac1041fcb559395da45a547
Miguel Alex Cantu <miguel.can2@gmail.com>
2019-10-03 15:16:30 -0500

    Added create and detail serializer + fixes
    This commit addes the WashesCreateSerializer and WashDestailSerializer
    for serializering data for their corresponding create/detial Views

--- carwash/washes/serializers.py
+++ carwash/washes/serializers.py
@@ -1,16 +1,31 @@
 # The washes serializers
 
-# from django.contrib.auth.models import User
+from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import Washes
 
 
 class WashesSerializer(serializers.ModelSerializer):
-    tools = serializers.StringRelatedField(
+    # Should we be using nested serializers here?
+    tools = serializers.PrimaryKeyRelatedField(
             many=True,
             read_only=True)
 
     class Meta:
-
         model = Washes
+        fields = ('id', 'title', 'date_created', 'tools', 'owner')
+
+
+class WashesCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Washes
+        # Owner is injected at the time of creation by overriding
+        # the perfrom_create() method in the corresponding view.
+        # It's set to the current user that's authenticated
         fields = ('id', 'title', 'date_created', 'tools')
+
+
+class WashDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Washes
+        fields = ('id', 'title', 'date_created', 'tools', 'owner')

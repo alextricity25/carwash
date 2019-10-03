66da44aba4773349ab573c6e8af384ead7e5a659
Miguel Alex Cantu <miguel.can2@gmail.com>
2019-10-03 15:18:30 -0500

    Add create and detail views
    This commit adds create and detail generic views and sets
    their corresponding serializer classes.
    
    It also adds the create-wash route and the detail route

--- carwash/washes/urls.py
+++ carwash/washes/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('create-wash/', views.WashesCreate.as_view(), name='washes-create'),
     path('', views.WashesList.as_view()),
+    path('<pk>/', views.WashesDetail.as_view(), name='washes-detail'),
 ]
--- carwash/washes/views.py
+++ carwash/washes/views.py
@@ -2,11 +2,9 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated 
 
 from .models import Washes
-from .serializers import WashesSerializer
-
+from .serializers import WashesSerializer, WashDetailSerializer, WashesCreateSerializer
 
 class WashesList(generics.ListAPIView):
-    permission_classes = [IsAuthenticated]
     serializer_class = WashesSerializer
 
     def get_queryset(self):
@@ -16,3 +14,16 @@ class WashesList(generics.ListAPIView):
         """
         user = self.request.user
         return Washes.objects.filter(owner=user)
+
+
+class WashesCreate(generics.CreateAPIView):
+    serializer_class = WashesCreateSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+class WashesDetail(generics.RetrieveAPIView):
+    serializer_class = WashDetailSerializer
+
+    def get_object(self):
+        return Washes.objects.get(pk=self.kwargs['pk'])
